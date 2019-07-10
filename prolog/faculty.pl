ad06f049890cf01723353d9ea22210bdc1f1304c
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-07-10 00:21:31 -0300

    Organize code & add get of all workes w/ same job

--- prolog/faculty.pl
+++ prolog/faculty.pl
@@ -1,22 +1,24 @@
-% Jorge é professor e diretor da faculdade. Possui doutorado e tem 60 anos de
-% idade. Ana é professora, possui mestrado e tem 45 anos. Cláudio é
-% professor, possui 23 anos e cursou a graduação. Regina é coordenadora e
-% professora, tem 37 anos e também possui a graduação. Júlio era professor
-% e possui mestrado, mas se aposentou e possui 80 anos. Joana é professora,
-% tem mestrado e 53 anos.
+% Jorge é professor de direito e diretor da faculdade. Possui doutorado e tem 60
+% anos de idade. Ana é professora de direito, possui mestrado e tem 45 anos.
+% Cláudio é professor de administração, possui 23 anos e cursou a graduação.
+% Regina é coordenadora e professora de administração, tem 37 anos e também
+% possui a graduação. Júlio era professor de economia e possui mestrado, mas se
+% aposentou e possui 80 anos. Joana é professora de economia, tem mestrado e 53
+% anos. Viviane era professora de economia, possui mestrado e tem 61 anos.
+
+% Geraldo, Vinicius e Maria são funcionários técnicos administrativos. Geraldo
+% possui 19 anos, Vinicius possui 41 e Maria possui 32. Maria é a
+% coordenadora dos técnicos administrativos. 
+
+% O diretor da faculdade recebe um bônus de 3000 reais. Diretores não podem
+% ser coordenadores simultaneamente.
+
+% O salário para técnicos é igual, no valor de 2300 reais.
+% O coordenador dos técnicos recebe um adicional de 700 reais.
 
 % O salário de professores doutores é de 8000 reais, o de mestres é de 6000 e
 % o de graduados é de 3000. O coordenador dos professores recebe um adicional de 
 % 1000 reais.
-
-% Geraldo, Vinicius e Maria são funcioáarios técnico-administrativos. Geraldo
-% possui 19 anos, Vinicius possui 41 e Maria possui 32. Maria é a
-% coordenadora dos técnicos administrativos. O salário para técnicos é igual,
-% no valor de 2300 reais. O coordenador dos técnicos recebe um adicional
-% de 700 reais.
-
-% O diretor da faculdade recebe um bônus de 3000 reais. Diretores não podem
-% ser coordenadores simultaneamente.
 
 % Professores tiram férias em janeiro, fevereiro e junho. Coordenadores tiram
 % férias em janeiro e fevereiro. Diretores tiram férias em janeiro.
@@ -27,50 +29,55 @@
 
 %% Funcoes
 % Professores
-funcao(professor, homem, jorge, 60, doutorado).
-funcao(professor, mulher, ana, 45, mestrado).
-funcao(professor, homem, claudio, 23, mestrado).
-funcao(professor, mulher, regina, 37, graduacao).
-funcao(professor, homem, julio, 80, graduacao).
-funcao(professor, mulher, joana, 53, mestrado).
+funcao(professor, direito, homem, jorge, 60, doutorado).
+funcao(professor, direito, mulher, ana, 45, mestrado).
+funcao(professor, administracao, homem, claudio, 23, mestrado).
+funcao(professor, administracao, mulher, regina, 37, graduacao).
+funcao(professor, economia, homem, julio, 80, graduacao).
+funcao(professor, economia, mulher, joana, 53, mestrado).
+funcao(professor, economia, mulher, viviane, 61, mestrado).
 
 % TAs
-funcao(tecnico_administrativo, homem, geraldo, 19, _).
-funcao(tecnico_administrativo, homem, vinicius, 41, _).
-funcao(tecnico_administrativo, mulher, maria, 32, _).
+funcao(tecnico_administrativo, _, homem, geraldo, 19, _).
+funcao(tecnico_administrativo, _, homem, vinicius, 41, _).
+funcao(tecnico_administrativo, _, mulher, maria, 32, _).
 
 % Diretor e Coordenadores
-diretor(N, F) :- funcao(F, _, N, _, _), N = jorge.
-coordenador(N, F) :- funcao(F, _, N, _, _), N = regina.
-coordenador(N, F) :- funcao(F, _, N, _, _), N = maria.
+diretor(N, F) :- funcao(F, _, _, N, _, _), N = jorge.
+coordenador(N, F) :- funcao(F, _, _, N, _, _), N = regina.
+coordenador(N, F) :- funcao(F, _, _, N, _, _), N = maria.
 
 %% Salarios
 % Professor com doutorado
-salario(N, 8000) :- funcao(professor, _, N, _, doutorado), coordenador(C, professor), diretor(D, _), C \= N, D \= N.
-salario(N, 9000) :- funcao(professor, _, N, _, doutorado), coordenador(N, professor).
-salario(N, 11000) :- funcao(professor, _, N, _, doutorado), diretor(N, _).
+salario(N, 8000) :- funcao(professor, _, _, N, _, doutorado), coordenador(C, professor), diretor(D, _), C \= N, D \= N.
+salario(N, 9000) :- funcao(professor, _, _, N, _, doutorado), coordenador(N, professor).
+salario(N, 11000) :- funcao(professor, _, _, N, _, doutorado), diretor(N, _).
 
 % Professor com mestrado
-salario(N, 6000) :- funcao(professor, _, N, _, mestrado), coordenador(C, professor), diretor(D, _), C \= N, D \= N.
-salario(N, 7000) :- funcao(professor, _, N, _, mestrado), coordenador(N, professor).
-salario(N, 9000) :- funcao(professor, _, N, _, mestrado), diretor(N, _).
+salario(N, 6000) :- funcao(professor, _, _, N, _, mestrado), coordenador(C, professor), diretor(D, _), C \= N, D \= N.
+salario(N, 7000) :- funcao(professor, _, _, N, _, mestrado), coordenador(N, professor).
+salario(N, 9000) :- funcao(professor, _, _, N, _, mestrado), diretor(N, _).
 
 % Professor com graduacao
-salario(N, 3000) :- funcao(professor, _, N, _, graduacao), coordenador(C, professor), diretor(D, _), C \= N, D \= N.
-salario(N, 4000) :- funcao(professor, _, N, _, graduacao), coordenador(N, professor).
-salario(N, 6000) :- funcao(professor, _, N, _, graduacao), diretor(N, _).
+salario(N, 3000) :- funcao(professor, _, _, N, _, graduacao), coordenador(C, professor), diretor(D, _), C \= N, D \= N.
+salario(N, 4000) :- funcao(professor, _, _, N, _, graduacao), coordenador(N, professor).
+salario(N, 6000) :- funcao(professor, _, _, N, _, graduacao), diretor(N, _).
 
 % Tecnicos Administrativos
-salario(N, 2300) :- funcao(tecnico_administrativo, _, N, _, _), coordenador(C, tecnico_administrativo), diretor(D, _), C \= N, D \= N.
-salario(N, 3000) :- funcao(tecnico_administrativo, _, N, _, _), coordenador(N, tecnico_administrativo).
-salario(N, 5300) :- funcao(tecnico_administrativo, _, N, _, _), diretor(N, _).
+salario(N, 2300) :- funcao(tecnico_administrativo, _, _, N, _, _), coordenador(C, tecnico_administrativo), diretor(D, _), C \= N, D \= N.
+salario(N, 3000) :- funcao(tecnico_administrativo, _, _, N, _, _), coordenador(N, tecnico_administrativo).
+salario(N, 5300) :- funcao(tecnico_administrativo, _, _, N, _, _), diretor(N, _).
 
-% Condicoes de aposentadoria
-aposentado(N) :- funcao(_, homem, N, I, _), I > 65.
-aposentado(N) :- funcao(_, mulher, N, I, _), I > 60.
+%% Condicoes de aposentadoria
+aposentado(N) :- funcao(_, _, homem, N, I, _), I > 65.
+aposentado(N) :- funcao(_, _, mulher, N, I, _), I > 60.
 
-% Ferias
+%% Ferias
 ferias(N, [janeiro]) :- diretor(N, _).
 ferias(N, [janeiro, fevereiro]) :- coordenador(N, _).
-ferias(N, [janeiro, fevereiro, junho]) :- funcao(professor, _, N, _, _), diretor(D, _), coordenador(C, professor), D \= N, C \= N.
-ferias(N, [janeiro, junho]) :- funcao(tecnico_administrativo, _, N, _, _), diretor(D, _), coordenador(C, tecnico_administrativo), D \= N, C \= N.
+ferias(N, [janeiro, fevereiro, junho]) :- funcao(professor, _, _, N, _, _), diretor(D, _), coordenador(C, professor), D \= N, C \= N.
+ferias(N, [janeiro, junho]) :- funcao(tecnico_administrativo, _, _, N, _, _), diretor(D, _), coordenador(C, tecnico_administrativo), D \= N, C \= N.
+
+%% Todos
+todos_professores(P) :- findall(N, funcao(professor, _, _, N, _, _), P).
+todos_tecnicos_administrativos(T) :- findall(N, funcao(tecnico_administrativo, _, _, N, _, _), T).
